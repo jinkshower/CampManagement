40d101f5980ee6230ec7280558b3ebbee71948d1
newbe01 <pkwaa3@gmail.com>
2024-01-12 18:00:28 +0900

    implement update score
     - 수강생 ID 입력
     - 과목ID 입력
     - 회차,  점수 입력 및 수정
     - score 등급 자동계산 저장

--- src/main/java/org/CampManagement/service/ScoreService.java
+++ src/main/java/org/CampManagement/service/ScoreService.java
@@ -1,8 +1,18 @@
 package org.CampManagement.service;
 
 import org.CampManagement.store.ScoreStore;
+import org.CampManagement.store.SubjectStore;
 
 public class ScoreService {
-    private ScoreStore scoreStore;
+    private static ScoreStore scoreStore = new ScoreStore();
 
+    public boolean updateScore(int studentId, int subjectId, int round, int score) {
+
+
+        SubjectStore subjectStore = new SubjectStore();
+        String type = subjectStore.getType(studentId, subjectId);
+
+        scoreStore.updateScore(studentId, subjectId, round, score, type);
+        return true;
+    }
 }
